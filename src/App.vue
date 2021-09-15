2c70e38a5a558154dce6a632e359996071c91459
Harry B <harry@gethornet.com>
2021-09-15 10:20:51 +0200

    Change styling to dark mode

--- src/App.vue
+++ src/App.vue
@@ -20,12 +20,16 @@ export default {
 </script>
 
 <style>
+body {
+  background: #2c3e50;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #fefefe;
   margin-top: 60px;
 }
 </style>
